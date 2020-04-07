260fdb3c6bc0c5d5c68eb5ac3f88e657a2a95103
LEW <lsw5244@naver.com>
2020-04-07 21:03:36 +0900

    왼쪽카드 오른쪽0,0으로 옮기기
    CardMoveManager를 통해 inCardMove를 실행시킴

---
+++ Assets/Scripts/CardMoveManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardMoveManager : MonoBehaviour
+{
+
+    public Transform[] rightCardPos;
+    public Transform[] leftCardPos;
+
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit = new RaycastHit();
+
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if(Physics.Raycast(ray.origin, ray.direction, out hit))
+            {
+                if(hit.collider.gameObject.tag == "Card")
+                {
+                    Debug.Log("CARD!!!!");
+                    hit.collider.gameObject.GetComponent<inCardMove>().movePos = rightCardPos[0];
+                    
+                }
+            }
+        }
+
+
+
+    }
+
+  
+}
---
+++ Assets/Scripts/inCardMove.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class inCardMove : MonoBehaviour
+{
+    public Transform movePos;
+
+    void Update()
+    {
+        if(!(movePos == null))
+            transform.position = Vector3.Lerp(transform.position, movePos.position, 0.01f);
+    }
+
+}
