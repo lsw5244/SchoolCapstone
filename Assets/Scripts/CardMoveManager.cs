dbe0d8873f7e703cb748b27fa5e9a834192cee06
LEW <lsw5244@naver.com>
2020-04-12 19:39:59 +0900

    덱을 배열이 아닌 리스트로 관리하도록 바꿈
    덱을 리스트로 관리하도록 변경

--- Assets/Scripts/CardMoveManager.cs
+++ Assets/Scripts/CardMoveManager.cs
@@ -8,7 +8,14 @@ public class CardMoveManager : MonoBehaviour
     public Transform[] rightCardPos;
     public Transform[] leftCardPos;
 
-    public GameObject[] deck = new GameObject[3];
+    public List<GameObject> deck = new List<GameObject>();
+
+    public int maxDeckSize = 3;
+
+    private void Start()
+    {
+        deck.Clear();
+    }
 
     void Update()
     {
@@ -18,35 +25,32 @@ public class CardMoveManager : MonoBehaviour
 
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 
-            if(Physics.Raycast(ray.origin, ray.direction, out hit))
+            if (Physics.Raycast(ray.origin, ray.direction, out hit))
             {
-                if(hit.collider.gameObject.tag == "Card")
+                if (hit.collider.gameObject.tag == "Card")
                 {
                     Debug.Log("CARD!!!!");
                     //hit.collider.gameObject.GetComponent<inCardMove>().movePos = rightCardPos[0];
                     //1. 카드 덱에 넣기
                     //2. 카드 덱을 기준으로rightCardPos로 옮기기
-                    if(deck[deck.Length - 1] == null)  //덱 찬건지 확인
-                    {                 
-                        for (int i = 0; i < deck.Length; i++)
-                        {
-                            if (deck[i] == null)
-                            {
-                                deck[i] = hit.collider.gameObject;
-                                hit.collider.gameObject.GetComponent<inCardMove>().movePos = rightCardPos[i];
-                                break;
-                            }
-                        }
-                        
+                    if (deck.Count < maxDeckSize)  //덱 찬건지 확인
+                    {
+                        deck.Add(hit.collider.gameObject);//덱에 카드넣기
+                        //카드 움직이기
+                        hit.collider.gameObject.GetComponent<inCardMove>().movePos = rightCardPos[deck.IndexOf(hit.collider.gameObject)];
+                        //Debug.Log(deck.IndexOf(hit.collider.gameObject));
+                        hit.collider.gameObject.tag = "DeckCard";
                     }
-
+                }
+                else if (hit.collider.gameObject.tag == "DeckCard")
+                {
+                    Debug.Log("DeckCardClick!!!!!!!!!!!");
                 }
             }
+
         }
-
-
-
     }
 
-  
+
+
 }
