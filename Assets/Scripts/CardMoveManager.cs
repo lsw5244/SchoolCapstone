e85a2f9d979a1338aa6324c3f56fa9d5eada4932
LEW <lsw5244@naver.com>
2020-04-12 19:04:41 +0900

    카드 왼쪽에서 오른족으로 순서대로 배치되도록 함
    카드 오른쪽 배치

--- Assets/Scripts/CardMoveManager.cs
+++ Assets/Scripts/CardMoveManager.cs
@@ -8,6 +8,8 @@ public class CardMoveManager : MonoBehaviour
     public Transform[] rightCardPos;
     public Transform[] leftCardPos;
 
+    public GameObject[] deck = new GameObject[3];
+
     void Update()
     {
         if(Input.GetMouseButtonDown(0))
@@ -21,8 +23,23 @@ public class CardMoveManager : MonoBehaviour
                 if(hit.collider.gameObject.tag == "Card")
                 {
                     Debug.Log("CARD!!!!");
-                    hit.collider.gameObject.GetComponent<inCardMove>().movePos = rightCardPos[0];
-                    
+                    //hit.collider.gameObject.GetComponent<inCardMove>().movePos = rightCardPos[0];
+                    //1. 카드 덱에 넣기
+                    //2. 카드 덱을 기준으로rightCardPos로 옮기기
+                    if(deck[deck.Length - 1] == null)  //덱 찬건지 확인
+                    {                 
+                        for (int i = 0; i < deck.Length; i++)
+                        {
+                            if (deck[i] == null)
+                            {
+                                deck[i] = hit.collider.gameObject;
+                                hit.collider.gameObject.GetComponent<inCardMove>().movePos = rightCardPos[i];
+                                break;
+                            }
+                        }
+                        
+                    }
+
                 }
             }
         }
