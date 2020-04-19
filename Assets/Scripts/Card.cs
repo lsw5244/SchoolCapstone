77953f3e8cb88bd1770aa33d288cb9073cff1255
LEW <lsw5244@naver.com>
2020-04-19 17:09:33 +0900

    Card클레스 작성 및 ChangeRedMat작성
    카드 만들기 전 실행 및 임시로 카드 사용 법 만듬

---
+++ Assets/Scripts/Card.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Card
+{
+    //카드 번호
+    public int number;
+    //카드 이름
+    public string titile;
+
+
+    public Card()
+    {
+
+    }
+
+    public virtual void Effect()
+    {
+        Debug.Log("Effcet 미작성");
+    }
+   
+}
---
+++ Assets/Scripts/ChangeRedMat.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeRedMat : MonoBehaviour
+{
+    Card card = new Card();
+
+    void Start()
+    {
+        card.number = 1;
+        card.titile = "ChangeRedMat";
+
+        //card.Effect();
+    }
+
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            Effect();
+            Debug.Log(card.number);
+            Debug.Log(card.titile);
+        }
+    }
+
+    public void Effect()
+    {
+        GameObject[] objs = GameObject.FindGameObjectsWithTag("Enermy");
+        foreach(GameObject obj in objs)
+        {
+            obj.GetComponent<MeshRenderer>().material.color = Color.red;
+        }
+    }
+
+
+}
