ce8330a57764525dd836cc55ac6cdf16fb6dc100
LEW <lsw5244@naver.com>
2020-04-30 15:53:55 +0900

    ActiveEx1 스키립트 임시 작성
    스크립트 임시작성

---
+++ Assets/Scripts/Card/ActiveEx1.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActiveEx1 : Card
+{
+    public GameObject particleEffect;
+    public float range;
+
+    void Start()
+    {
+        number = 2;
+        title = "IceAge";
+        status = 0;
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit = new RaycastHit();
+
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray.origin, ray.direction, out hit))
+            {
+                GameObject Eff = Instantiate(particleEffect, hit.point, Quaternion.identity);
+                Destroy(Eff, 1.5f);
+
+                Collider[] hitColl = Physics.OverlapSphere(hit.point, range);
+
+                StartCoroutine("SpreadEffect", hitColl);
+
+            }
+        }
+    }
+
+    IEnumerator SpreadEffect(Collider[] colls)
+    {
+        foreach(Collider coll in colls)
+        {
+            if(coll.tag == "Enermy")
+            {
+                coll.GetComponent<MeshRenderer>().material.color = Color.green;
+                yield return new WaitForSeconds(0.3f);
+            }
+
+        }
+    }
+}
